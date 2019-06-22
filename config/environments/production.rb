1823689387060c91bc18e592f04ae33fce3ebe2f
Hoang Nguyen <phuonghoang713@gmail.com>
2019-06-22 20:36:06 +0700

    update config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,8 @@ Rails.application.configure do
 
 
   # Store files on Amazon S3.
-  config.active_storage.service = :amazon
-
+  #config.active_storage.service = :amazon
+  config.active_storage.service = :local
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
+  #get 'hello_world', to: 'hello_world#index'
   resources :photos
   get '/photos' => 'photos#index'
 
