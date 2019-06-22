479f61e70d38d909577ef8d096ce9d6e90acde23
Hoang Nguyen <phuonghoang713@gmail.com>
2019-06-22 21:52:43 +0700

    add edit button

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-100.times do
+10.times do
   photo = Photo.create(
     username: Faker::Internet.user_name,
     caption: Faker::Lorem.sentence,
